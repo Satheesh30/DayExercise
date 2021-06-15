11d5a6d17f8587aca67a14c3dd8a6eccade90bac
satheesh kumar p <psk30198@gmail.com>
2021-06-15 22:57:51 +0530

    fisrt commit

---
+++ dayexericse/src/day12/GoodBrush.java
@@ -0,0 +1,33 @@
+package day12;
+
+public class GoodBrush {
+
+	public static void main(String[] args) {
+		GoodPaintBrush brush=new GoodPaintBrush();
+		RedPaint rp=new RedPaint();
+		BluePaint bp=new BluePaint();
+		GreenPaint gp=new GreenPaint();
+		
+		brush.rp=gp;
+		brush.doPaint();
+	}
+}class GoodPaintBrush{
+	Paint rp;
+	public void doPaint() {
+		System.out.println(rp);
+	}
+
+	}
+class Paint{
+	
+}
+class RedPaint extends Paint{
+	
+}
+class BluePaint extends Paint{
+	
+}
+class GreenPaint extends Paint{
+	
+}
+
---
+++ dayexericse/src/day12/MultipleInherDemo.java
@@ -0,0 +1,24 @@
+package day12;
+
+public class MultipleInherDemo {
+
+	public static void main(String[] args) {
+		Satheesh obj=new Satheesh();
+		
+	}
+}
+class LivingObject{
+	
+}
+class Human extends LivingObject{
+	
+}
+class Dracula{
+	
+}
+class Dog{
+	
+}class Satheesh extends Human{
+	
+}
+

fca67313a9b14459543744fac77a5156cbc9c827
satheesh kumar p <psk30198@gmail.com>
2021-06-24 14:33:54 +0530

    committ

--- dayexericse/src/day16/ReflectionDemo.java
+++ dayexericse/src/day16/ReflectionDemo.java
@@ -5,7 +5,7 @@ public class ReflectionDemo {
 public static void main(String[] args)throws Exception {
 		
 		//1. static binding.... things which cannot be changed..
-		Paint staticPaint=new RedPaint();
+		Paint staticPaint=new RedPaint("redpaint");
 		System.out.println("Object created through static way...:"+staticPaint);
 		
 		//2. dynamic binding.... To dynamically create objects...
@@ -14,12 +14,12 @@ public static void main(String[] args)throws Exception {
 		String paintClass=scan.next();
 		//Where ever the return type of a method is Object, you need to do typecasting
 		
-		Paint dynamicPaint=(Paint)Class.forName(paintClass).newInstance();
-		System.out.println("Object created through dynamic way...:"+dynamicPaint);
+	//	Paint dynamicPaint=(Paint)Class.forName(paintClass).newInstance();
+	//	System.out.println("Object created through dynamic way...:"+dynamicPaint);
 		
 		//3. new way of creating objects dynamically....after jdk9
-		Paint s=(Paint)Class.forName(paintClass).getConstructor().newInstance();
-		System.out.println("Object created through new dynamic way...:"+dynamicPaint);
+		Paint s=(Paint)Class.forName(paintClass).getConstructor(String.class).newInstance("paint is ");
+		System.out.println("Object created through new dynamic way...:"+s);
 		
 	
 	}
@@ -27,13 +27,23 @@ public static void main(String[] args)throws Exception {
 	
 }
 class RedPaint extends Paint{
-	public RedPaint() {
-		
+	String name;
+	public RedPaint(String name) {
+		this.name=name;
+	}
+	@Override
+	public String toString() {
+		return this.name+"RedPaint 32";
 	}
 }
 class BluePaint extends Paint{
-	public BluePaint() {
-		
+	String name;
+	public BluePaint(String name) {
+		this.name=name;
+	}
+	@Override
+	public String toString() {
+			return this.name+"BluePaint 33";
 	}
 }
 

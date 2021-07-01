1e7dac743e211911824f111c576b6899850fdb88
satheesh kumar p <psk30198@gmail.com>
2021-07-01 09:10:12 +0530

    commit

---
+++ dayexericse/src/day23/OptimisticLock.java
@@ -0,0 +1,43 @@
+package day23;
+
+public class OptimisticLock {
+
+	public static void main(String[] args) {
+		CounsellingHall university=new CounsellingHall();
+		Thread subramain=new Thread(new CounsellingJob(university),"subramain");
+		Thread josh=new Thread(new CounsellingJob(university),"josh");
+		subramain.start();
+		josh.start();
+	}
+}
+class CounsellingJob implements Runnable{
+	public CounsellingJob(CounsellingHall university) {
+		this.university=university;
+	}
+	CounsellingHall university;
+	@Override
+	public void run() {
+	//	synchronized (university) {
+		
+		if(Thread.currentThread().getName().equals("subramain")) {
+			university.table1();
+			university.table2();
+		}
+		else if(Thread.currentThread().getName().equals("josh")){
+			university.water();
+		}
+	//	}
+	}
+}class CounsellingHall{
+	synchronized public void table1() {
+		System.out.println("entered table1...:"+Thread.currentThread().getName());
+		try {Thread.sleep(5000);}catch(Exception e) {}
+	}
+	synchronized public void table2() {
+		System.out.println("entered table2...:"+Thread.currentThread().getName());
+	}
+	public void water() {
+		System.out.println("entered water area to drink water....:"+Thread.currentThread().getName());
+	}
+
+}
